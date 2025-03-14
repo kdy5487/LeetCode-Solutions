98e84678fa22c3da00d7de17c0d2cd16331d3b16
kdy5487 <142620707+kdy5487@users.noreply.github.com>
2025-03-14 09:28:35 +0900

    Time: 0 ms (100%), Space: 148.4 MB (89.26%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.dart
@@ -0,0 +1,19 @@
+class Solution {
+  int removeDuplicates(List<int> nums) {
+    if (nums.isEmpty) {
+        return 0;//배열 비었으면 0
+    }
+    int j = 1; 
+    for(int i = 1; i < nums.length; i++)
+    {
+        if(nums[i] == nums[j-1]){//같으면 j안바뀌고 순회하면서 계속 비교
+            continue;
+        } 
+        else{
+            nums[j] = nums[i]; //다르면 기준점 이동.
+            j++;
+        }
+    }
+    return j;
+  }
+}
